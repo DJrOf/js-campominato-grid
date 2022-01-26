6d154047c5c1ea21ba269d2b67dfbd8fcc65045a
DJrOf <danieljrofosu@gmail.com>
2022-01-26 19:03:40 +0100

    last-fix.done

--- js-campominato-grid/js/script.js
+++ js-campominato-grid/js/script.js
@@ -8,19 +8,27 @@ con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, 
 la cella cliccata si colora di azzurro. */ 
 
+// BombsNo gen
+
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-// Bomb's number generator
+
+
+// Gioca - CTA Button -
 
 const play = () => {
+
+// Reset txt-btn
     playButton.innerText = 'Ricomincia';
 
+// Grid reset    
     const grid = document.getElementById('grid');
     grid.innerHTML = '';
 
-    let attemps = 0;
+// #Select Difficulty    
+    let attempts = 0;
     const TOTAL_BOMBS = 16;
 
-    const level = document.getElementById('Level').Value;
+    const level = document.getElementById('level').value;
 
     let totalCells;
     let cellsPerRow;
@@ -33,31 +41,31 @@ const play = () => {
             totalCells = 49;
             break;
             default:
-                totalCells = 81;
-    }
-
+            totalCells = 81;
+    }       
     cellsPerRow = Math.sqrt(totalCells);
 
-    const MAX_ATTEMPS = totalCells - TOTAL_BOMBS;
+    const MAX_ATTEMPTS = totalCells - TOTAL_BOMBS;
 
     //  Functions
 
-// Bombs
-    const generateBombs = (TOTAL_BOMBS, totalCells) => {
+    // Bomb gen
+
+    const generateBombs = (totalBombs, totalNumbers) => {
         const bombs = [];
         while (bombs.length < totalBombs) {
             const randNumber = getRandomNumber(1, totalNumbers);
             if (!bombs.includes(randNumber)) bombs.push(randNumber);
         }
         return bombs;
-};
+    };
 
-// Celle
+    // Cells gen
 const generateCell = (number, cellsPerRow) => {
     const cell = document.createElement('div');
     cell.className = 'cell';
-    cell.id = number;
-    const sideLength = `cell( 100% / ${cellsPerRow} )`;
+    cell.innerText = number;
+    const sideLength = `calc(100% / ${cellsPerRow})`;
     cell.style.width = sideLength;
     cell.style.height = sideLength;
     return cell;
@@ -68,17 +76,19 @@ const generateCell = (number, cellsPerRow) => {
 const gameOver = (bombs, points, hasLost) => {
 
     showBombs(bombs);
-
     const messageElement = document.createElement('h3');
     messageElement.className = 'message';
     const messageText = hasLost ? `Pai perso! Punti: ${points}`:
     `Hai vinto! Punti: ${points}`
 };
 
+
 //Click - Evento
 
 const onCellClick = (clickedCell, bombs, number) => {
     clickedCell.removeEventListener('click', onCellClick);
+
+    number = clickedCell.innerText;
 
     if (bombs.includes(number)) {
         gameOver(bombs, attempts, true);
@@ -89,11 +99,9 @@ const onCellClick = (clickedCell, bombs, number) => {
         if (attempts === MAX_ATTEMPTS)
         gameOver(bombs, attempts, false)
     }
-
 };
 
 //Show bombs, block click
-
 const showBombs = (bombs) => {
     const cells = document.querySelectorAll('.cell');
     for (let i = 0; i < cells.length; i++) {
@@ -102,31 +110,24 @@ const showBombs = (bombs) => {
     cell.removeEventListener ('click', oneCellClick);
     if (bombs.includes(cellNumber)) cell.classical.add('bomb');
     }
-};
+};  
 
-// Griglia
-
+    // Grid gen
 const generateGrid = (cellsNumber, cellsPerRow, bombs) => {
     for (let i = 1; i <= cellsNumber; i++) {
         const cell = generateCell(i, cellsPerRow);
 
-        cell.addEventListener('click', (e) => oneCellClick(e.target, bombs, i))
-
+        cell.addEventListener('click', (e) => onCellClick(e.target, bombs, i))
         grid.appendChild(cell);
     }
 };
 
-
+// Exec
+const bombs = generateBombs(TOTAL_BOMBS, totalCells);
 generateGrid(totalCells, cellsPerRow, bombs);
 
-// Play button funzione 
+};
 
-
-const playbutton = document.getElementById('play');
-playButton.addEventListener('click', play);
-playButton = document.getElementById('play');
-
-}
-
-
-
+ // Play button funzione 
+ const playButton = document.getElementById('play');
+ playButton.addEventListener('click', play);
